8c816a3bc78f2e4260d716ff1e10b19ef2e12bfe
osm0718 <osm0718@gmail.com>
2019-05-05 12:27:04 -0400

    Updated JAVA file and reproduceability README file

--- bin/IVTheoretical.java
+++ bin/IVTheoretical.java
@@ -7,13 +7,28 @@ import org.math.plot.*;
 import org.math.plot.plotObjects.*;
 import java.util.Arrays;
 
-public class termProject
+public class IVTheoretical
 {
+	//Tunnel current constants
+	public static double vP = 0.05;		//Peak voltage
+	public static double iP = 4.;		//Peak current density
+
+	//Excess current constants
+	public static double vV = 0.37;		//Valley voltage
+	public static double iV = 0.0037;	//Valley current density
+	public static double ecp = 30;		// Excess current prefactor
+
+	//Thermal current constants
+	public static double scd = 1e-16;	//Saturation current density
+	public static double q = 1.602e-19;	//Electron charge
+	public static double k = 1.38e-23;	//Boltzmann's constant
+	public static double t = 150;		//Tempeprature
+
 	public static void main(String[] args)
 	{
 		boolean checkPlotIV = true;
 
-		// Open file and store data for I-V graphs
+		// Open file and store data (I-V graphs for individual currents and total current)
 		PrintWriter outputFile = null;
   	  	try
   	  	{
@@ -28,79 +43,44 @@ public class termProject
   	  	// Set max index
   	  	double dv = 0.002;	// Time Step
   	  	double vMax = 0.55;	// Final time
-  	  	int iMax = (int)((vMax/dv));
+  	  	int iMax = (int)((vMax/dv));	
 
 		// Declare arrays
 		double[] v = new double[iMax];		// Voltage
-		double[] iTot = new double[iMax];	// Total current
+		double[] iTD = new double[iMax];	// Total current
 
 		// Set initial conditions
 		v[0] = 0;
-		iTot[0] = iTheo(v[0]);
+		iTD[0] = curretnTD(v[0]); // Current across tunnel diode
 	
 		// Theoreitcal values
 		for(int i=1;i<(iMax);i++)
 		{
 			v[i] = v[i-1] + 0.002;
 
-			iTot[i] = iTheo(v[i]);
+			iTD[i] = curretnTD(v[i]);
 		}
 
 		// Create live plot
 		if (checkPlotIV)
-			plotIV(v, iTot);
+			plotIV(v, iTD);
 
 		//Storing info in a file to use later if needed
 		for(int i=0;i<iMax;i++)
 		{
-			outputFile.println(v[i] + "	" + iTot[i]);
+			outputFile.println(v[i] + "	" + iTD[i]);
 		}	
 
 		// Close files
 		outputFile.close();
 	} // main
 
-		public static double iTheo (double v)
+		public static double curretnTD (double v)
 	{
-		double vP = 0.05;
-
-		double currentTotal = (v*itTheo(v)/vP) + ixTheo(v) + ithTheo(v);
+		double currentTotal = v * iP * Math.pow(Math.E, (1-v/vP))/vP + iV * Math.pow(Math.E, (ecp*(v-vV))) + scd*Math.pow(Math.E, (v*q/k/t)-1);
 
 		return currentTotal;
-	} // iTheo
-
-	public static double itTheo (double v)
-	{
-		double vP = 0.05;
-		double iP = 4.;
-
-		double iT = iP * Math.pow(Math.E, (1-(v/vP)));
-
-		return iT;
-	} // itTheo
-
-	public static double ixTheo (double v)
-	{
-		double vV = 0.37;
-		double iV = 0.0037;
-		double ecp = 30;
-
-		double iX = iV * Math.pow(Math.E, (ecp*(v-vV)));
-
-		return iX;
-	} // ixTheo
-
-	public static double ithTheo (double v)
-	{
-		double scd = 1e-16;
-		double q = 1.602e-19;
-		double k = 1.38e-23;
-		double t = 150;
-
-		double iTH = scd * Math.pow(Math.E, ((v*q)/(k*t))-1);
-
-		return iTH;
-	} // ithTheo
+	} // currentTD
 
 	public static void plotIV(double[] volts, double[] iTOT)
 	{
@@ -110,9 +90,9 @@ public class termProject
 		plot.addLinePlot("Total current", volts, iTOT);
 
 		// Axes
-		plot.setAxisLabel(0, "Volts (millivolts)");
+		plot.setAxisLabel(0, "Volts");
 		plot.getAxis(0).setLabelPosition(0.5, -0.1);
-		plot.setAxisLabel(1, "Current (milliamps)");
+		plot.setAxisLabel(1, "Current");
 
 		// Title
 		BaseLabel title = new BaseLabel ("Total Current vs Voltage", Color.BLACK, 0.5, 1.1);
--- bin/README.md
+++ bin/README.md
@@ -1,3 +1,10 @@
 # Code
 
 Place the code you developped for the term project in this folder. Add any instructions/documentation required to run the code, and what results can be expected in this *README* file.
+
+How to reproduce my results:
+- Run the java file "IVTheoretical.java"
+    - This will produce an I-V graph of a tunnel diode based on pre-exisiting equations developped by a SPICE
+       behavioral model (there will also be a text files with all of the values needed to make this graph)
+- Run the java file "TDOscillator.java"
+    - This is will produce a V vs t graph of a tunnel diode oscilator produced by a set of ODEs
